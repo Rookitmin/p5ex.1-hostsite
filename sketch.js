bf5db8a72502dc2e46d243e7963a94459941c76f
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2018-10-01 14:45:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,17 +4,19 @@ function setup() {
 var xSpeed1=0.1;
 var xSpeed2=0; 
 var x = 200;
+fill(0,0,0);
 function draw() {
   background(220);
-	fill(0,0,0);
 	ellipse(mouseX - 5,mouseY - 5,10,10);
 	ellipse(x,200,5,5);
 	xSpeed2 += xSpeed1;
 	x += xSpeed2;
 	if (x > 250) {
 		xSpeed1 = -0.1;
+		fill(mouseX,mouseY,x);
 	}
 	if (x < 150) {
 		xSpeed1 = 0.1;
+		fill(mouseY,mouseX,x);
 	}
 }
